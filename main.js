fe02cf39116df66f9a787634f331345b68598a2c
pawan-live <pawan.senpura@gmail.com>
2022-07-03 01:18:10 +0530

    add validation to login with name

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("main.js connected");
+var username;
+
+let continueBtn = document.getElementById("continue-btn");
+
+// SEQUENCE //
+
+continueBtn.addEventListener("click", readUsername);
+
+// FUNCTIONS //
+
+// console.log but shorter
+
+// read username on click continueBtn
+function readUsername() {
+  let readVal = document.getElementById("username-field").value;
+  if (readVal == "") {
+    loginError("Enter your name");
+  } else {
+    username = readVal;
+    console.log(username);
+  }
+}
+
+function shake(object) {
+  // code here
+}
+
+function loginError(message) {
+  let notifArea = document.getElementById("notif-area");
+  notifArea.innerHTML = "Enter your name";
+}
