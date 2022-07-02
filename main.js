e3068f262830bd71abedfa6472c8ee9d42ce0070
pawan-live <pawan.senpura@gmail.com>
2022-07-03 03:02:00 +0530

    shake on error interaction

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+// Variables //
 var username;
 
-let continueBtn = document.getElementById("continue-btn");
+// DOM constants //
+const continueBtn = document.getElementById("continue-btn");
 
 // SEQUENCE //
 
@@ -8,13 +10,16 @@ continueBtn.addEventListener("click", readUsername);
 
 // FUNCTIONS //
 
-// console.log but shorter
-
 // read username on click continueBtn
 function readUsername() {
+  let loginSection = document.getElementById("login-section");
+
+  // read name from field
   let readVal = document.getElementById("username-field").value;
   if (readVal == "") {
     loginError("Enter your name");
+    // shake on error
+    shake(loginSection);
   } else {
     username = readVal;
     document.getElementById("select-section").style.display = "block";
@@ -22,10 +27,19 @@ function readUsername() {
   }
 }
 
+// shake object on error
+// pass the object to shake as argument
 function shake(object) {
-  // code here
+  object.classList.add("shake");
+  setTimeout(function () {
+    if (object.classList.contains("shake")) {
+      object.classList.remove("shake");
+    }
+  }, 400);
 }
 
+// display error msg on notif area
+// in login section
 function loginError(message) {
   let notifArea = document.getElementById("notif-area");
   notifArea.innerHTML = "Enter your name";
