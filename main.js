e430254299da4949362a8ea965956472802ddd92
pawan-live <pawan.senpura@gmail.com>
2022-07-03 04:25:42 +0530

    add select details, add validation interactions

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ var username;
 
 // DOM constants //
 const continueBtn = document.getElementById("continue-btn");
+const detailsBtn = document.getElementById("details-continue-btn");
 
 // SEQUENCE //
 
 continueBtn.addEventListener("click", readUsername);
+detailsBtn.addEventListener("click", readDetails);
 
 // FUNCTIONS //
 
@@ -22,10 +24,28 @@ function readUsername() {
     shake(loginSection);
   } else {
     username = readVal;
-    let selectSection = document.getElementById("select-section");
-    selectSection.style.display = "block";
+    let detailsSection = document.getElementById("details-section");
+    detailsSection.style.display = "flex";
     document.getElementById("login-section").style.display = "none";
-    fadeIn(selectSection);
+    fadeIn(detailsSection);
+  }
+}
+
+// read details
+function readDetails() {
+  let detailsSection = document.getElementById("details-section");
+
+  const options = []; //array to hold options
+  options[0] = document.getElementById("select-fac").value;
+  options[1] = document.getElementById("select-year").value;
+  options[2] = document.getElementById("select-grp").value;
+
+  for (i = 0; i < options.length; i++) {
+    if (options[i] == 0) {
+      shake(detailsSection);
+      console.log("done");
+      break;
+    }
   }
 }
 
@@ -52,4 +72,9 @@ function shake(object) {
 // fade in
 function fadeIn(object) {
   object.classList.add("fade-in");
+  setTimeout(function () {
+    if (object.classList.contains("fade-in")) {
+      object.classList.remove("fade-in");
+    }
+  }, 400);
 }
