7662b414f4a86b4543f1e85dcfa1ed1d6b4d1ef9
pawan-live <pawan.senpura@gmail.com>
2022-07-03 17:25:04 +0530

    complete css greeting card

--- main.js
+++ main.js
@@ -107,6 +107,23 @@ function setCookie(cname, cvalue, exdays) {
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+// get a cookie by name
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 // display error msg on notif area
 // in login section
 function loginError(message) {
