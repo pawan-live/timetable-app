83ac3af656d1a01588b99fc64905e2bc734fa4d0
pawan-live <pawan.senpura@gmail.com>
2022-07-08 01:10:39 +0530

    added sample dataset to simulate fetch data from database

--- dataset.js
+++ dataset.js
@@ -9,17 +9,59 @@ var metaData = {
   weather: "sunny",
 };
 
-var data = {
-  faculty: {
-    IT: {
+var table = {
+  // metadata
+  key: "0001",
+  added_by: "your name here",
+  last_updated: "7/8/2022",
+  // timetable content
+  day: {
+    // monday
+    monday: {
       1: {
-        //   sample data here
+        module: "OOP",
+        type: "lecture",
+        loc: "N3A Lab",
+        start: "10:30",
+        end: "12:30",
       },
       2: {
-        //   2nd year here
+        module: "SE",
+        type: "lab",
+        loc: "A506 Lab",
+        start: "12:30",
+        end: "14:00",
       },
       3: {
-        //   hjhhjhj
+        module: "DSE",
+        type: "lecture",
+        loc: "A507 Hall",
+        start: "15:00",
+        end: "17:30",
+      },
+    },
+    // tuesday
+    tuesday: {
+      1: {
+        module: "SE",
+        type: "lab",
+        loc: "N3A Lab",
+        start: "10:30",
+        end: "12:30",
+      },
+      2: {
+        module: "OOP",
+        type: "lecture",
+        loc: "A506 Hall",
+        start: "12:30",
+        end: "14:00",
+      },
+      3: {
+        module: "DSE",
+        type: "lecture",
+        loc: "A507 Hall",
+        start: "15:00",
+        end: "17:30",
       },
     },
   },
