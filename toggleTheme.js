61b41000107b8d5adeb6fa29111c1f11400cefcc
Russell Peiris <ardpeiris@gmail.com>
2022-08-01 17:40:33 +0530

    dark media queries added

--- toggleTheme.js
+++
@@ -1,9 +0,0 @@
-function toggleTheme(){
-    var theme = document.getElementById('css-file');
-
-    if (theme.getAttribute('href') == "style.css"){
-      theme.setAttribute('href', "dark.css");
-    }else{
-      theme.setAttribute('href', "style.css");
-    }
-  }
